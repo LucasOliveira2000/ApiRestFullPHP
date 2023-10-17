7c49332c6ae8ad8777e09aa5e5156f9dce3a3841
LucasOliveira2000 <j-lucas9@hotmail.com>
2023-10-17 15:06:25 -0300

    Mudança nas Regras

--- app/Http/Controllers/CadastroCliente.php
+++ app/Http/Controllers/CadastroCliente.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Cadastro;
 use Illuminate\Http\Request;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
 
 
 
@@ -17,17 +17,40 @@ class CadastroCliente extends Controller
 
     public function store(Request $request)
     {
-        $request ->validate([
-            'nome' => 'required|unique:clientes|max:100|min:4',
-            'endereco' => 'required',
-            'cidade' => 'required', 
-            'cep' => 'required',
-            'telefone' => 'required|max:11|min:11',
-        ]);
-        $cadastro = Cadastro::create($request->all());
-        return response()->json($cadastro, 201);
-    }  
-
+        try {
+            $request->validate([
+                'nome' => 'required|unique:clientes|max:100|min:4',
+                'endereco' => 'required',
+                'cidade' => 'required', 
+                'cep' => 'required',
+                'telefone' => 'required|max:11|min:11',
+            ], [
+                'nome.required' => 'O campo nome é obrigatório.',
+                'nome.unique' => 'O nome já está em uso.',
+                'nome.max' => 'O nome não pode ter mais de 100 caracteres.',
+                'nome.min' => 'O nome deve ter pelo menos 4 caracteres.',
+                'endereco.required' => 'O campo endereço é obrigatório.',
+                'cidade.required' => 'O campo cidade é obrigatório.',
+                'cep.required' => 'O campo CEP é obrigatório.',
+                'telefone.required' => 'O campo telefone é obrigatório.',
+                'telefone.max' => 'O telefone deve ter no máximo 11 caracteres.',
+                'telefone.min' => 'O telefone deve ter no mínimo 11 caracteres.',
+                // Adicione outras mensagens conforme necessário.
+            ]);
+    
+            $cadastro = Cadastro::create($request->all());
+    
+            return response()->json($cadastro, 201);
+    
+        } catch (ValidationException $e) {
+            $errors = $e->validator->errors()->getMessages();
+    
+            return response()->json(['error' => $errors], 404);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Erro interno no servidor.'], 500);
+        }  
+    }    
+    
     public function show($id)
     {
         try {
@@ -62,7 +85,6 @@ class CadastroCliente extends Controller
         } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
             return response()->json(['error' => 'Cadastro não encontrado.'], 404);
         }
-
     }
 
     public function destroy($id)
